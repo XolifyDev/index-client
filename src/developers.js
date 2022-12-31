d573a739a8269d2c3d0235ef80d47e30791f2b06
IamAdren <IamAdren@iamadrenn@gmail.com>
2022-12-30 22:39:18 -0500

    INdex updates'

--- src/developers.js
+++ src/developers.js
@@ -4,13 +4,13 @@ import DiscordSolutions from './assets/partners/discordSolutions.png';
 export const Developers = [
 	{
 		name: 'LOD Scripts',
-		src: 'https://lod.fivem.design',
+		src: 'https://lod.fivem.design/',
 		discord: 'https://discord.gg/kCtjBVWYHE',
 		logo: LODPng,
 	},
 	{
 		name: 'Discord Solutions',
-		src: 'https://discordsolutions.fivem.design',
+		src: 'https://discordsolutions.fivem.design/',
 		discord: 'https://discord.com/invite/DrTwxdfGhr',
 		logo: DiscordSolutions,
 	},
