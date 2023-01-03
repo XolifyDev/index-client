97dc4a512253afce5e792e2fd8610df1701cd337
IamAdren <IamAdren@iamadrenn@gmail.com>
2023-01-02 20:24:04 -0500

    App rename

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './app';
+import App from './App';
 
 import { UserAuthContextProvider } from './contexts';
 
